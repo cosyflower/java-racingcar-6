573bca5f3ed4b4457ed7d0f4177c40f2f0abbcdc
sunghun <nokdonwithkj211@gmail.com>
2023-11-25 08:56:59 +0900

    test(RacingRefereeTest): 라운드를 진행하고, 우승자의 이름을 출력하는 테스트 작성

--- src/test/java/refactor/RacingRefereeTest.java
+++ src/test/java/refactor/RacingRefereeTest.java
@@ -1,2 +1,53 @@
-package refactor;public class RacingRefereeTest {
+package refactor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import refactor.numberGenerator.RandomNumberGenerator;
+import refactor.racer.ParticipatedRacers;
+import refactor.racer.Racer;
+import refactor.racer.RacerName;
+import refactor.racer.RacerPosition;
+
+public class RacingRefereeTest {
+    @Test
+    void flow() {
+        ParticipatedRacers participatedRacers = ParticipatedRacers.from(
+                Arrays.asList(Racer.of(RacerName.from("성훈"), RacerPosition.from(1)),
+                        Racer.of(RacerName.from("혜림"), RacerPosition.from(1)),
+                        Racer.of(RacerName.from("상호"), RacerPosition.from(1)),
+                        Racer.of(RacerName.from("선응"), RacerPosition.from(1)))
+        );
+
+        ParticipatedRacers moved = RacingReferee.executeEachRound(participatedRacers,
+                () -> new RandomNumberGenerator().generateNumber());
+        Map<String, Integer> eachRoundStatus =
+                RacingReferee.getEachRoundStatus(moved);
+
+        for (String key : eachRoundStatus.keySet()) {
+            System.out.println("key = " + key);
+            System.out.println("eachRoundStatus.get(key) = " + eachRoundStatus.get(key));
+        }
+    }
+
+    @Test
+    void winnerNames() {
+        ParticipatedRacers participatedRacers = ParticipatedRacers.from(
+                Arrays.asList(Racer.of(RacerName.from("성훈"), RacerPosition.from(1)),
+                        Racer.of(RacerName.from("혜림"), RacerPosition.from(2)),
+                        Racer.of(RacerName.from("상호"), RacerPosition.from(2)),
+                        Racer.of(RacerName.from("선응"), RacerPosition.from(3)))
+        );
+
+        ParticipatedRacers moved = RacingReferee.executeEachRound(participatedRacers, () -> 4);
+        Map<String, Integer> eachRoundStatus =
+                RacingReferee.getEachRoundStatus(moved);
+        List<String> winnerNames = RacingReferee.getWinnerNames(moved);
+
+        for (String winnerName : winnerNames) {
+            System.out.println("winnerName = " + winnerName);
+        }
+
+    }
 }
